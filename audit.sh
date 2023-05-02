0b05b02c19e70298d2c2e0ac20f272a7a8c16451
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-02 20:51:43 +0300

    Test

--- audit.sh
+++ audit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 pages=$1
 
 response=$(curl -s -X POST "https://api.omnifractal.com/v1/auditWithActions" \
   -H "Content-Type: application/json" \
   -d "{\"pages\": $pages}")
  
-echo "{audit_result}={$response}" >> $GITHUB_OUTPUT
+echo "{audit_result}=$response" >> $GITHUB_OUTPUT
