78d77d9a5f8aefba66516ef1e43cb6f0c8897f03
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-02 20:59:20 +0300

    T

--- audit.sh
+++ audit.sh
@@ -4,6 +4,8 @@ set -e
 
 pages=$1
 
+echo "Data being sent: {\"pages\": $pages}"
+
 response=$(curl -s -X POST "https://api.omnifractal.com/v1/auditWithActions" \
   -H "Content-Type: application/json" \
   -d "{\"pages\": $pages}")
