f0937b508bfb974a67a6d3186bae999f0bf60a70
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-02 19:51:51 +0300

    Update

--- audit.sh
+++ audit.sh
@@ -5,7 +5,6 @@ api_key=$2
 
 response=$(curl -s -X POST "https://api.omnifractal.com/v1/auditWithActions" \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $api_key" \
   -d "{\"pages\": $pages}")
 
 echo "::set-output name=audit_result::$response"
