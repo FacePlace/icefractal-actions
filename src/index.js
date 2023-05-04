c16d61c7628450424900e7e8dd66293f654001f7
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-04 21:22:58 +0300

    BB X

--- src/index.js
+++ src/index.js
@@ -4,32 +4,40 @@ const fs = require('fs');
 const path = require('path');
 
 (async function() {
-  const pages = core.getInput('pages').split('\n').map((page) => page.trim());
+  try {
+    const pages = core.getInput('pages').split('\n').map((page) => page.trim());
+    
+    // const budget = core.getInput('budget');
+    // console.log(JSON.parse(budget))
+    
+    const budgetPath = core.getInput('budgetPath');
+    const budget = budgetPath ? 
+    JSON.parse(fs.readFileSync(budgetPath, 'utf8')) :
+    undefined;
+    
+    console.log(pages);
+    console.log(budget);
+    
+    const apiKey = core.getInput('apiKey');
 
-  // const budget = core.getInput('budget');
-  // console.log(JSON.parse(budget))
-
-  const budgetPath = core.getInput('budgetPath');
-  const budget = JSON.parse(fs.readFileSync(budgetPath, 'utf8'));
-
-  console.log(pages);
-  console.log(budget);
-
-  const apiKey = core.getInput('apiKey');
-
-  await axios.post(
-    'https://api.omnifractal.com/v1/auditWithActions',
-    {
-      pages: [
-        ...pages,
-      ],
-      budget: budget,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
+    await axios.post(
+      'https://api.omnifractal.com/v1/auditWithActions',
+      {
+        pages: pages,
+        budget: budget,
       },
-    },
-  );
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      },
+      )
+      .catch((error) => {
+        console.log(error.response.data);
+        core.setFailed(error.response.data);
+      });
+  }  catch (error) {
+    core.setFailed(error.message);
+  }
 })();
