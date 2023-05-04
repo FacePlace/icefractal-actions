7dcc08341bccf44bb70754147d42fe68f05548ba
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-04 19:34:35 +0300

    BG

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
 const axios = require('axios');
+const fs = require('fs');
 
 (async function() {
   const pages = core.getInput('pages').split('\n').map((page) => page.trim());
+  const budgetPath = core.getInput('budgetPath');
+  const budget = JSON.parse(fs.readFileSync(budgetPath, 'utf8'));
   console.log(pages);
+  console.log(budget)
   const apiKey = core.getInput('apiKey');
 
   await axios.post(
@@ -12,6 +16,7 @@ const axios = require('axios');
       pages: [
         ...pages,
       ],
+      budget: budget,
     },
     {
       headers: {
