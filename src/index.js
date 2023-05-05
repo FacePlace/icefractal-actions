447455ac6e98b0c6d21ca504fe75251733f25ef9
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-05 13:38:46 +0300

    CC

--- src/index.js
+++ src/index.js
@@ -1,42 +1,51 @@
 const core = require('@actions/core');
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
 
-(async function() {
+(async function () {
   try {
     const pages = core.getInput('pages').split('\n').map((page) => page.trim());
-    
-    // const budget = core.getInput('budget');
-    // console.log(JSON.parse(budget))
-    
+
     const budgetsPath = core.getInput('budgetsPath');
-    const budgets = budgetsPath ? 
-      JSON.parse(fs.readFileSync(budgetsPath, 'utf8')) :
-      undefined;
-    
+    const budgets = budgetsPath
+      ? JSON.parse(fs.readFileSync(budgetsPath, 'utf8'))
+      : undefined;
+
     const apiKey = core.getInput('apiKey');
 
-    console.log('budgets', budgets);
+    // Retrieve additional information
+    const branch = core.getInput('branch');
+    const repository = core.getInput('repository');
+    const commit_sha = core.getInput('commit_sha');
+    const commit_message = core.getInput('commit_message');
+    const commit_author = core.getInput('commit_author');
+    const commit_author_email = core.getInput('commit_author_email');
 
-    await axios.post(
-      'https://api.omnifractal.com/v1/auditWithActions',
-      {
-        pages: pages,
-        budgets: budgets,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
+    await axios
+      .post(
+        'https://api.omnifractal.com/v1/auditWithActions',
+        {
+          pages: pages,
+          budgets: budgets,
+          branch: branch,
+          repository: repository,
+          commit_sha: commit_sha,
+          commit_message: commit_message,
+          commit_author: commit_author,
+          commit_author_email: commit_author_email,
         },
-      },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
       )
       .catch((error) => {
         console.log(error);
         core.setFailed(error.response.data);
       });
-  }  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 })();
