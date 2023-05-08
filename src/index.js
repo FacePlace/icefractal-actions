bcf1859aaedf05a4e1acf5cdcac6829cfe8fb759
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2023-05-08 11:15:53 +0300

    BER

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ const fs = require('fs');
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          core.setFailed(`Error: ${error.response.status} - ${error.response.message || error.response.statusText || `Something went wrong`}`);
+          core.setFailed(`Error: ${error.response.status} - ${error.response.data.message || error.response.message || error.response.statusText || `Something went wrong`}`);
           console.error('Error details:', error.response.data);
         } else if (error.request) {
           // The request was made but no response was received
@@ -70,7 +70,7 @@ const fs = require('fs');
           console.error('Error details:', error.request);
         } else {
           // Something happened in setting up the request that triggered an Error
-          core.setFailed(`Error: ${error.response.message || error.response.statusText || `Something went wrong`}`);
+          core.setFailed(`Error: ${error.response.data.message || error.response.message || error.response.statusText || `Something went wrong`}`);
           console.error('Error details:', error);
         }
       });
